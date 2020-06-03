e8271dc5d0d9daa8b1c31d2ba84ee7a0ac8d6d2e
cstayyab <cstayyab@gmail.com>
2020-06-04 03:23:00 +0500

    Added API Key to Config.js (Unsafe)

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const Config = {
     api_url: "http://115.186.176.141:8080/",
     brandText: "offensive mamba",
-    vulners_api_url: "https://vulners.com/api/v3/"
+    vulners_api_url: "https://vulners.com/api/v3/",
+    vulners_api_key: "2MOSL67KZ4YF6OYPWP5JDRXZOWIT9Q97PFOJFESA1IWNTL6JNQJG8UX7MXADA6NW"
 }
 
 export default Config;
